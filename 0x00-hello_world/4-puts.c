ca2da16e20e43e1e28e6965cd3c8e613f324aa7c
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-09-08 21:15:52 -0700

    Added my first program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * put.c: the code prints (Programming is like building a multilingual puzzle)
+ *
+ * Retrun: 0 if no error, return non-zero if errors
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
