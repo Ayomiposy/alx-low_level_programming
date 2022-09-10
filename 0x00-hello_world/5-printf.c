5a6f0ded61f0d29b36a06d03828386443055dda2
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-09-10 01:40:39 -0700

    Added my first program that printt with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - defines the entry point for the prpogram
+ *
+ * Return: return 0 if errors, if not return non zero if no errors
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
