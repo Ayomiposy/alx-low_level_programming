1d8077edf660dc84108f33787159284c1752e940
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-09-12 01:36:13 -0700

    Advance task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/**
+ * main - Defines the entry point of the program
+ *
+ * Description: the program will print:
+ * (and that piece of art is useful" - Dora Korpar, 2015-10-19)
+ *
+ * Return: Always return 1.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	if (printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"))
+	{
+	}
+	return (1);
+}
