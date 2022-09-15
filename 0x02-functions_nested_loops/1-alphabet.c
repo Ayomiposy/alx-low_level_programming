cf2b1a7be1be6fea58ac7b390d195f2630d5d5ba
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-09-14 18:17:16 -0700

    Advance task on C, print with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * Description: Print alphabet a to z in lower case
+ */
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+
+		_putchar(i);
+		_putchar('\n);
+}
