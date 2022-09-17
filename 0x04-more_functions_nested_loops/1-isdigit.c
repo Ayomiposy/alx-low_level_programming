c26ba9e3368164521e60e747b11fdb0a31931844
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-09-16 17:58:31 -0700

    Mandatory task on c, _isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
- * _isdigit - check the code
- *
- * Description: write a function that checks for a digit
- * 0 through 9
- *
- * Return: return 1 otherwise 0
+ * _isdigit - Check for digits (0 through 9)
+ * @c: int type param
+ * Return: 1 if digit, else 0
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c > 47 && c < 58)
+		return (1);
 
-	return (1);
-	
 	return (0);
 }
+
