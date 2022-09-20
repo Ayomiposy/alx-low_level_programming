0f31c676e0122161fb645c1b0a47d194061d01ca
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-09-20 10:18:39 -0700

    Mandatory task on c, 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * @a: the first integer to be swapped
+ *
+ * @b: the second integer to be swapped
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+	*a = *b;
+	*b * a;
+}
