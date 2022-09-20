6e1d226b1777fdd7acdc14a2e4d1b58a8b425e75
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-09-20 10:54:55 -0700

    Mandatory task on c, 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * strlen - return the length of a string
+ *
+ * @s: the string pointer
+ *
+ * Return: the length of string
+ *
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
