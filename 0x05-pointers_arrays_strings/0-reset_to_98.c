adc16c5d829cf3d2018980036259d24f467ee0e0
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-09-20 10:00:34 -0700

    Mandatory task on c, 1-swap.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take pointer to an int as parameter
+ * and update the value it points to be 98
+ *
+ * @n: The pointer to an int
+ *
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
