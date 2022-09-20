aed5e621184e41fa1c36527d66fc9888facc206d
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-09-20 11:07:38 -0700

    Mandatory task on c, 3-put.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: the string pointer
+ *
+ * Return: the length of string
+ *
+ */ 
+
+void _puts(char *str);
+{
+        _putchar(strlen(s));
+	_putchar('\n');
+}
+
