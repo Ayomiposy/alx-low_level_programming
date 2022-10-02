1120406500505fbcafef5efdfa48511f0bd68b98
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-10-02 16:53:26 -0700

    Mandatory task, program prints its name followed by a new line (argc and argv)

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program prints it's name followed by new line
+ *
+ * @argc: counts arguments
+ * @argv: is the arguments counted
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", agrv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,9 @@
+## 0x0A. C - argc, argv
+
+argc - is a variable of type integer that counts the number of argument passed in the command line
+argv - is a one dimensional array that holds the strigs of argc. 
+
+## task
+
+
+## My Idea
