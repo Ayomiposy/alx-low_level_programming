e6b8b957109abae1ec8012dc04b20b198aae3884
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-10-02 17:19:05 -0700

    Mandatory task, program prints the number of arguments (argc and argv)

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Program prints number of argument
+ * @argc: Counts argument
+ * @argv: the arguments
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0:)
+}
