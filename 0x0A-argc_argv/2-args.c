e7d311d633657c46974ece029e59112aa684136d
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-10-03 13:45:12 -0700

    Mandatory task, program prints all arguments it receives (argc and argv)

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,9 +12,13 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	for (i=0; i < argc; i++)
+
+	if (argc > 0)
+	{
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
+	}
 	}
 	return (0);
 }
