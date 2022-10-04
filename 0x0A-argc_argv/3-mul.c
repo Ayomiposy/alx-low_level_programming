832565fb1c9c873504d843d9d232d265392398de
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-10-04 04:03:26 -0700

    Mandatory task, program multiplies two numbers (argc and argv)

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,13 +16,10 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 	i = atoi(argv[1]);
-
 	j = atoi(argv[2]);
-
 	printf("%d\n", i * j);
 	return (0);
 	}
-	
 	printf("Error\n");
 	return (1);
 }
