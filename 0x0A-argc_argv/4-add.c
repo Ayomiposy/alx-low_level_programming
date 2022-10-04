f5efd0f1758c6cdd2826ac3735df5e5c50e36b10
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-10-04 09:14:57 -0700

    Mandatory task, add positive integers (argc and argv)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,13 +13,13 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	int i;
+	int i, j;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; j < argc; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (argv[i][j] < 0)
 			{
 			printf("Error\n");
 			return (1);
