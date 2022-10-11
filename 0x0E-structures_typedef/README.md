2a179c9516535ac5964e9387e96a4fa9ebb2be94
George Joshua <georgejoshuaayomiposi@gmail.com>
2022-10-11 16:30:48 -0700

    Mandatory task, structures and typedef, dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+## structures and typedef
+
+## Structure - Is a user defined data type that is used to collect different types of data type under one name
+
+## typedef - Is a keyword use to create personalised data type
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+
+/**
+ * struct dog - structure for dog
+ * @name: attribute pointer to char
+ * @owner: attribute pointer to data type char
+ * age: attribute pointer to data type float
+ */
+
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner
+};
+
